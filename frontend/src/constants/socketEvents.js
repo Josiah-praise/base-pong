dea6deefabc5235085e040809a794e2ad3f2f8ac
Josiah Praise <praisechinonso21@gmail.com>
2025-12-09 22:52:10 +0100

    feat(frontend): expose leaderboard event list

--- frontend/src/constants/socketEvents.js
+++ frontend/src/constants/socketEvents.js
@@ -2,3 +2,8 @@ export const SOCKET_EVENTS = {
   LEADERBOARD_UPDATE: 'leaderboardUpdate',
   RANKINGS_UPDATE: 'rankingsUpdate'
 };
+
+export const LEADERBOARD_EVENTS = [
+  SOCKET_EVENTS.LEADERBOARD_UPDATE,
+  SOCKET_EVENTS.RANKINGS_UPDATE
+];
