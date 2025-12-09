49fa669cee173099ad140ad0661caecb9d3b2f31
Josiah Praise <praisechinonso21@gmail.com>
2025-12-09 19:04:08 +0100

    refactor(frontend): use backend url resolver in constants

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,3 +1,5 @@
+import { resolveBackendUrl } from './utils/backendUrl';
+
 // Game constants
 export const STORAGE_KEY = 'pong_username';
 
@@ -7,4 +9,4 @@ export const PADDLE_SPEED = 0.01;
 export const INITIAL_RATING = 1000;
 
 // Backend connection
-export const BACKEND_URL = process.env.REACT_APP_BACKEND_URL; 
+export const BACKEND_URL = resolveBackendUrl();
