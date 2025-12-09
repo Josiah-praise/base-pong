cc687ce323d782eed7234d51c68aac0a6bf0bfde
Josiah Praise <praisechinonso21@gmail.com>
2025-12-09 22:47:13 +0100

    feat(backend): add helper to list leaderboard events

--- backend/src/constants/socketEvents.js
+++ backend/src/constants/socketEvents.js
@@ -1,4 +1,14 @@
-module.exports = {
+const SOCKET_EVENTS = {
   LEADERBOARD_UPDATE: 'leaderboardUpdate',
   RANKINGS_UPDATE: 'rankingsUpdate'
+};
+
+const getLeaderboardEvents = () => [
+  SOCKET_EVENTS.LEADERBOARD_UPDATE,
+  SOCKET_EVENTS.RANKINGS_UPDATE
+];
+
+module.exports = {
+  SOCKET_EVENTS,
+  getLeaderboardEvents
 };
