c942872af21b74f29116a5f46c06d476dd14d4ba
Josiah Praise <praisechinonso21@gmail.com>
2025-12-09 22:52:58 +0100

    feat(frontend): hook for subscribing to leaderboard aliases

--- frontend/src/hooks/useLeaderboardUpdates.js
+++ frontend/src/hooks/useLeaderboardUpdates.js
@@ -1,1 +1,20 @@
-export const useLeaderboardUpdates = () => {};
+import { useEffect } from 'react';
+import { LEADERBOARD_EVENTS } from '../constants/socketEvents';
+
+export const useLeaderboardUpdates = (socket, onUpdate) => {
+  useEffect(() => {
+    if (!socket || typeof onUpdate !== 'function') {
+      return undefined;
+    }
+
+    LEADERBOARD_EVENTS.forEach((eventName) => {
+      socket.on(eventName, onUpdate);
+    });
+
+    return () => {
+      LEADERBOARD_EVENTS.forEach((eventName) => {
+        socket.off(eventName, onUpdate);
+      });
+    };
+  }, [socket, onUpdate]);
+};
