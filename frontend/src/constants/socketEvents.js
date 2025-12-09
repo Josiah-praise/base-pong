27ce49bfb1b1b446f7a3eff38c0dc7bf13305fee
Josiah Praise <praisechinonso21@gmail.com>
2025-12-09 23:08:37 +0100

    chore(frontend): freeze socket event constants

--- frontend/src/constants/socketEvents.js
+++ frontend/src/constants/socketEvents.js
@@ -1,7 +1,7 @@
-export const SOCKET_EVENTS = {
+export const SOCKET_EVENTS = Object.freeze({
   LEADERBOARD_UPDATE: 'leaderboardUpdate',
   RANKINGS_UPDATE: 'rankingsUpdate'
-};
+});
 
 export const LEADERBOARD_EVENTS = [
   SOCKET_EVENTS.LEADERBOARD_UPDATE,
